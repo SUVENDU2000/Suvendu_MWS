edd9321ff42e1a4c4c7418c7481692effc179124
SUVENDU2000 <suvendukumars19@gmail.com>
2023-07-08 12:52:46 +0530

    first commit

---
+++ MaybeLLine/Model/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MaybeLLine
+//
+//  Created by Suvendu Kumar on 08/07/23.
+//
+
+import Foundation
---
+++ MaybeLLine/View/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  MaybeLLine
+//
+//  Created by Suvendu Kumar on 08/07/23.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
