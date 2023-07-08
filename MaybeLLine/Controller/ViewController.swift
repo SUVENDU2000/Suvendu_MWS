0b0c7e361ca27e092264aec8b9cdd520dd49ccbd
SUVENDU2000 <suvendukumars19@gmail.com>
2023-07-08 12:55:06 +0530

    First Commit

--- MaybeLLine/Controller/ViewController.swift
+++ MaybeLLine/Controller/ViewController.swift
@@ -11,11 +11,76 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var MyTableView: UITableView!
     
+    var data: [MakeupProduct] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetch()
         
+        MyTableView.rowHeight = 420
         
+        MyTableView.register(UINib(nibName: "MyTableViewCell", bundle: nil), forCellReuseIdentifier: "MyTableViewCell")
         
+    }
+    func fetch() {
+        fetchData{ result, error in
+            guard let result = result else{
+                return
+            }
+            self.data = result
+            print(result)
+            DispatchQueue.main.async {
+                self.MyTableView.reloadData()
+            }
+            
+        }
     }
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableViewCell", for: indexPath) as! MyTableViewCell
+        cell.FifthLabel.text = data[indexPath.row].name
+        cell.SecondLabel.text = data[indexPath.row].description
+        cell.ThirdLabel.text = data[indexPath.row].price
+     //   cell.ForthLabel.text = data[indexPath.row].rating
+        cell.FifthLabel.text = data[indexPath.row].brand
+        cell.UserImage.load(url: URL(string: data[indexPath.row].image_link)!)
+        
+        
+        
+        return cell
+    }
+    
+}
+
+extension ViewController {
+    func fetchData(completion: @escaping ([MakeupProduct]?, Error?) -> Void) {
+        let url = URL(string: "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline")!
+        var request = URLRequest(url: url, timeoutInterval: 20)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            guard let data = data else {
+                completion(nil, error)
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let products = try decoder.decode([MakeupProduct].self, from: data)
+                completion(products, nil)
+            } catch {
+                completion(nil, error)
+            }
+            
+        }
+        task.resume()
+    }
+}
--- MaybeLLine/View/MyTableViewCell.swift
+++ MaybeLLine/View/MyTableViewCell.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class MyTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var UserImage: UIImageView!
+    @IBOutlet weak var FirstLabel: UILabel!
+    
+    @IBOutlet weak var SecondLabel: UILabel!
+    
+    @IBOutlet weak var ThirdLabel: UILabel!
+    
+    @IBOutlet weak var ForthLabel: UILabel!
+    
+    @IBOutlet weak var FifthLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,7 +29,29 @@ class MyTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
+}
+struct MakeupProduct: Codable {
+    let description: String
+    let id: Int
+    let brand: String
+    let name: String
+    let price: String
+    let image_link: String
+    let rating: Double?
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
